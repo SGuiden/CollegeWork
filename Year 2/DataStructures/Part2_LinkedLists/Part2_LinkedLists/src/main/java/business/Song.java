66bf22e51ddc699ccabd6a8d397d0cf573d887a5
Guiden <shaunguiden2003@gmail.com>
2024-03-06 22:25:17 +0000

    updated

--- Year 2/DataStructures/Part2_LinkedLists/Part2_LinkedLists/src/main/java/business/Song.java
+++ Year 2/DataStructures/Part2_LinkedLists/Part2_LinkedLists/src/main/java/business/Song.java
@@ -4,52 +4,74 @@ package business;
  *
  * @author michelle
  */
-package utils;
-
 import java.util.Objects;
-
-/**
- * @author michelle
- */
+//Implement a class named Song. A Song is made up of two String fields: an artist and a title. Your Song class should
+//provide the following functionality:
+//1. A default constructor that sets the artist to “Joe Bloggs” and title to “Mmmbop”.
 public class Song {
     private String artist = "Joe Bloggs";
     private String title = "Mmmbop";
-    public Song(String Artist, String Title){
+
+    //2. A constructor that takes in two parameters – the artist and the title.
+    public Song(String Artist, String Title) {
         this.artist = Artist;
         this.title = Title;
     }
 
+    //3. Getter and setter methods for the artist and title.
+
+    /**
+     * gets the name of artist
+     *
+     * @return the artist String
+     */
     public String getArtist() {
         return artist;
     }
 
+    /**
+     * gets the name of the song
+     *
+     * @return the title string
+     */
     public String getTitle() {
         return title;
     }
 
+
+    //4. An equals() method and a hashCode() method – these should base their calculation on the artist and title fields.
+
+    /**
+     * @param o object to check
+     * @return a boolean indicating either the object
+     */
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Song song)) return false;
+        if (this == o) {
+            return true
+        }
+        if (!(o instanceof Song song)) {
+            return false
+        }
         return Objects.equals(getArtist(), song.getArtist()) && Objects.equals(getTitle(), song.getTitle());
     }
 
+    /**
+     * @return
+     */
     @Override
     public int hashCode() {
         return Objects.hash(artist, title);
     }
 
+    //5. A toString() method that returns the artist and title information in a well-formatted manner.
+
     @Override
+    /**
+     * @return a String containing the title and artist in a user friendly manner
+     */
     public String toString() {
         return "Now Playing - " + title + " by " + artist;
     }
 
-//Implement a class named Song. A Song is made up of two String fields: an artist and a title. Your Song class should
-//provide the following functionality:
-//1. A default constructor that sets the artist to “Joe Bloggs” and title to “Mmmbop”.
-//2. A constructor that takes in two parameters – the artist and the title.
-//3. Getter and setter methods for the artist and title.
-//4. An equals() method and a hashCode() method – these should base their calculation on the artist and title fields.
-//5. A toString() method that returns the artist and title information in a well-formatted manner.
-
-
+}
--- Year 2/DataStructures/Part2_LinkedLists/Part2_LinkedLists/src/main/java/utils/LinkedList.java
+++ Year 2/DataStructures/Part2_LinkedLists/Part2_LinkedLists/src/main/java/utils/LinkedList.java
@@ -13,6 +13,7 @@ public class LinkedList {
     //should maintain a tail reference, as well as a reference to the first element. The following functionality should be
     //provided:
 
+    //1. A method called size() that takes no parameters & returns the number of Songs currently stored in your list.
     /**
      * returns the size of the LinkedList
      * @return numElements, a varaible counting how many elements are in the list
@@ -20,16 +21,28 @@ public class LinkedList {
     public int size(){
         return numElements;
     }
-    //1. A method called size() that takes no parameters & returns the number of Songs currently stored in your list.
+
+    //2. A method called get() that takes one parameter, an int representing the position from which data should be
+    //retrieved. The position should be validated and invalid positions handled accordingly.
+    //If the position is valid, the method should return the data (a Song) in the position specified.
+
+    /**
+     * returns the Song in the given position
+     * @param pos the position of the object you want
+     * @return the object in that position
+     */
     public String get(int pos){
         if(pos >=0 && pos <= tail) {
             return LinkedList[pos].toString();
         }
-        else return "invalid Position";
+        else {return "invalid Position";}
     }
-    //2. A method called get() that takes one parameter, an int representing the position from which data should be
-    //retrieved. The position should be validated and invalid positions handled accordingly.
-    //If the position is valid, the method should return the data (a Song) in the position specified.
+
+    /**
+     * retuns the index of the song
+     * @param song the song you want to search for
+     * @return either the postition of the song or a -1 for indexout of bounds
+     */
     public static int indexOf(Song song){
         for(int i = 0; i <= tail; i++){
             if(song == LinkedList[i]){
@@ -38,9 +51,15 @@ public class LinkedList {
         }
         return -1;
     }
-    //3. A method called indexOf() that takes one parameter, a Song to be found in the list. The method should
-    //return an int indicating the position at which the Song could be found. If the Song could not be found, an
-    //appropriate value should be returned to indicate this.
+
+
+    //4. A method called add() that takes a single parameter, a Song to be added to the end of the list.
+
+    /**
+     * adding the song to the array
+     * @param song - the song you want to add
+     */
+
     public static void add(Song song){
         if(indexOf(song) == -1){
             grow();
@@ -51,13 +70,7 @@ public class LinkedList {
             System.out.println("Song Already Esists");
         }
     }
-    //4. A method called add() that takes a single parameter, a Song to be added to the end of the list.
-    public static void add(Song song){
-        if(indexOf(song) == -1){
-            grow();
-            LinkedList[numElements];
-        }
-    }
+
 
     //5. A method called isEmpty() that takes no parameters. This should return a boolean indicating if there is data
     //currently stored in the list.
@@ -81,9 +94,38 @@ public class LinkedList {
 
     //Your list should contain a method called removeAll() that takes one parameter:
     //The Song to be removed (this should not be null).
-    //All instances of this Song should be removed from the list. The method should return a boolean indicating if the Song was removed (if at least one instance is removed, the method should return true, otherwise it should return false).
+    //All instances of this Song should be removed from the list.
+    //The method should return a boolean indicating if the Song was removed
+    //(if at least one instance is removed, the method should return true, otherwise it should return false).
+    public static boolean removeAll(Song song){
+        //if the song is null return false
+        if(song == null){return false}
+        //count to the tail, if the song is found
+        for(int i = 0; i<tail; i++){
+            if((LinkedList[i].equals(song)){
+               counter++;
+            }
+        }
 
-public static void grow(){
+        //if counter is greater than 0 it will create a new list, add every instanstance that doesnt match and will return true, if it is 0 or less it will return false
+        if(counter > 0){
+        LinkedList newList = new LinkedList(numElements - counter);
+        this.numElements = 0;
+        for(int i = 0; i<tail; i++){
+            if(!(LinkedList[i].equals(song)){
+                newList[numElements] = LinkedList[i];
+                this.numElements++;
+            }
+        }
+        return true;
+        }
+        else{return false;}
+    }
+
+    /**
+     * growing an array by 1
+     */
+    public static void grow(){
         Song [] grownList = new Song[numElements+1];
         this.numElements++;
         LinkedList = grownList;
